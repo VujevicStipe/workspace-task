950adff7f502c2a79f25efd46c0e46aef31b2614
VujevicStipe <stipevujevicking@gmail.com>
2024-12-18 17:30:23 +0100

    scrollbar fix

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styles from '../page.module.css'
-import HeroSectionDestination from '../containers/hero-section-destination/HeroSectionDestination'
-import RouteHighlightsSection from '../containers/route-highlights-section/RouteHighlightsSection'
-import Step1Section from '../containers/step1-section/Step1Section'
-import Step2Section from '../containers/step2-section/Step2Section'
-import Step3Section from '../containers/step3-section/Step3Section'
-import ClosingCtaSection from '../containers/closing-cta-section/ClosingCtaSection'
+import React from "react";
+import styles from "./page.module.css";
+import HeroSectionDestination from "../containers/hero-section-destination/HeroSectionDestination";
+import RouteHighlightsSection from "../containers/route-highlights-section/RouteHighlightsSection";
+import Step1Section from "../containers/step1-section/Step1Section";
+import Step2Section from "../containers/step2-section/Step2Section";
+import Step3Section from "../containers/step3-section/Step3Section";
+import ClosingCtaSection from "../containers/closing-cta-section/ClosingCtaSection";
 
 export default function Destination() {
   return (
@@ -17,5 +17,5 @@ export default function Destination() {
       <Step3Section />
       <ClosingCtaSection />
     </div>
-  )
+  );
 }
